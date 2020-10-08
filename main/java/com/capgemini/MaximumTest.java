31ef398eeaed80e742d074b28722646caa50b2b6
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-09 00:10:38 +0530

    [Shreya]Added: printing the max using generic method

--- main/java/com/capgemini/MaximumTest.java
+++ main/java/com/capgemini/MaximumTest.java
@@ -21,14 +21,19 @@ public class MaximumTest<T extends Comparable<T>> {
 	public T MaxOfAll(T... values) {
 		Arrays.sort(values);
 		this.max = values[values.length - 1];
-		System.out.println("The max is :"+max);
+		printMax(max);
 		return max;
+	}
+	
+	public static <T> void printMax(T max) {
+		System.out.println("Max is : " + max);
 	}
 
 	public static void main(String[] args) {
 		
 		MaximumTest<Integer> i = new MaximumTest<Integer>();
 		i.MaxOfAll(1,2,3,4,5);
+		
 		MaximumTest<Float> f = new MaximumTest<Float>();
 		f.MaxOfAll(1.2f,2.2f,3.3f,4.4f);
 		MaximumTest<String> s=new MaximumTest<String>();
