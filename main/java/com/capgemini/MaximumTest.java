0a45cff37ef0a950f9289fe21c86a399ab4b67bb
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-08 22:47:17 +0530

    [Shreya]Added: genric method

--- main/java/com/capgemini/MaximumTest.java
+++ main/java/com/capgemini/MaximumTest.java
@@ -1,19 +1,12 @@
 package com.capgemini;
 
 import java.lang.*;
-import java.lang.Float;
+
 
 public class MaximumTest {
-	Float x,y,z;
-
-	public MaximumTest(Float x, Float y, Float z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
 	
-	public static Integer toFindMaxInteger(Integer x, Integer y, Integer z)  {
-		Integer max =x;
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+		T max =x;
 		if(y.compareTo(max)>0) {
 			max=y;
 		}
@@ -22,45 +15,22 @@ public class MaximumTest {
 		}
 		System.out.println("The max number is "+max);
 		return max;	
-	}
-	
-	public static Float toFindMaxFloat(Float i, Float j, Float k)  {
-		Float max =i;
-		if(j.compareTo(max)>0) {
-			max=j;
-		}
-		if(k.compareTo(max)>0) {
-			max=k;
-		}
-		System.out.println("The max number is "+max);
-		return max;	
-	}
-	
-	public static String toFindMaximum(String a,String b,String c) {
-		String max =a;
-		if(b.compareTo(max)>0) {
-			max=b;
-		}
-		if(c.compareTo(max)>0) {
-			max=c;
-		}
-		System.out.println("The max String is "+max);
-		return max;	
 		
 	}
+
 	public static void main(String[] args) {
 		Integer x = 10;
 		Integer y = 20;
 		Integer z = 30;
-		toFindMaxInteger(x,y,z);
+		maximum(x,y,z);
 		Float i = (float) 1.1;
 		Float j = (float) 2.1;
 		Float k = (float) 3.1;
 		String a ="Apple";
 		String b="banana";
 		String c ="peach";
-		toFindMaxFloat(i,j,k);
-		toFindMaximum(a,b,c);
+		maximum(i,j,k);
+		maximum(a,b,c);
 	}
 		
 }
--- test/java/com/capgemini/Testing.java
+++ test/java/com/capgemini/Testing.java
@@ -8,10 +8,9 @@ public class Testing {
 	@Test
 	public void testFindMax() {
 		
-		Assert.assertEquals(new Float(3.1), MaximumTest.toFindMaxFloat((float)1.1, (float)2.1, (float)3.1));
-		Assert.assertEquals("peach", MaximumTest.toFindMaximum("apple","banana","peach"));
-		
-		
+		Assert.assertEquals(new Integer(30), MaximumTest.maximum(10,20,30));
+		Assert.assertEquals(new Float(3.1), MaximumTest.maximum((float)1.1, (float)2.1, (float)3.1));
+		Assert.assertEquals("peach", MaximumTest.maximum("apple","banana","peach"));		
 	}
 
 }
